fb518fb563118dfb4fa1b350bc225431093f7ffa
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-01-12 23:14:26 +0100

    add the printReports and test it

--- lesson3-functions.go
+++ lesson3-functions.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+/*
+
+Assignment
+Complete the printReports function. It takes as input a sequence of messages, intro, body, outro. It should call printCostReport once for each message.
+
+For each call of printCostReport, give it an anonymous function that returns the cost of a message as an integer. Here are the costs:
+
+Intro: 2x the message length
+Body: 3x the message length
+Outro: 4x the message length
+Use the built-in len() function to get the length of a string:
+
+helloLen := len("hello")
+// helloLen = 5
+*/
+
 func getProductMessage(tier string) string {
 	quantityMsg, priceMsg, _ := getProductInfo(tier)
 	return "You get " + quantityMsg + " for " + priceMsg + "."
@@ -28,6 +44,34 @@ func getCoords() (x, y int) {
 	return // automatically returns x and y
 }
 
+func printReports(intro, body, outro string) int {
+	// Anonymous function for calculating costs
+	printCostReport(func(msg string) int {
+		return 2 * len(msg)
+	}, intro)
+
+	printCostReport(func(msg string) int {
+		return 3 * len(msg)
+	}, body)
+
+	printCostReport(func(msg string) int {
+		return 4 * len(msg)
+	}, outro)
+
+	return 0 // Return value is not specified in the prompt
+}
+
 func main() {
 	fmt.Println(getCoords())
+	printReports(
+		"Welcome to the Hotel California",
+		"Such a lovely place",
+		"Plenty of room at the Hotel California",
+	)
+}
+
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
+	fmt.Println()
 }
