26f27cb21dbbffc27024c6003467d23e20d0d0bf
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-02-17 22:05:42 +0100

    converting string to int and converting int to string using strconv

---
+++ convering.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	fmt.Println("Converting String to int using Atoi()")
+	var s string = "123a" // correct the error by removing 'a'   "123a" ---> "123"
+
+	i, err := strconv.Atoi(s)
+	fmt.Printf("%v , %T \n", i, i)
+	fmt.Printf("%v , %T \n", err, err)
+
+	fmt.Println("====================================================")
+
+	fmt.Println("Converting int to string using Itoa() ")
+	var a int = 244
+	st := strconv.Itoa(a)
+	fmt.Printf("%v , %T \n", st, st)
+}
